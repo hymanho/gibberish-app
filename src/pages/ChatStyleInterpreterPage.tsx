40c3ea1410be1f85451ad06f02d889328522cdea
1toldyou <86680163+1toldyou@users.noreply.github.com>
2024-04-23 19:31:29 -0700

    some styling on layout

--- src/pages/ChatStyleInterpreterPage.tsx
+++ src/pages/ChatStyleInterpreterPage.tsx
@@ -6,6 +6,7 @@ function ResultEntry({result}: {result: TranslationResult}){
     return (
         <div style={{
             border: "1px solid black",
+            margin: "0.5vh"
         }}>
             <p>{result.sourceLanguage} -&gt; {result.targetLanguage}</p>
             <p>{result.targetText}</p>
@@ -42,17 +43,43 @@ export default function ChatStyleInterpreterPage(){
     }
     
     return (
-        <div>
-            <h1>Interpreter Page (Chat-Style)</h1>
+        <div style={{
+            margin: 0,
+            padding: 0,
+        }}>
+            <section style={{
+                marginTop: "0vh",
+                height: "5vh",
+            }}>
+                <h1>Interpreter Page (Chat-Style)</h1>
+            </section>
             
             <hr/>
-            {results.map((result, index) => {
-                return <ResultEntry key={index} result={result}/>;
-            })}
+            
+            {/* TODO: auto scroll to the button when new entry added */}
+            <section style={{
+                height: "80vh",
+                overflowY: "scroll",
+            }}>
+                {(results.length === 0) ? (
+                    <p style={{textAlign: "center"}}>Press "Add Random Entry" to load some example</p>
+                ) : (
+                    results.map((result, index) => {
+                            return <ResultEntry key={index} result={result}/>;
+                    })
+                )}
+            </section>
+            
             <hr/>
             
-            <button onClick={addRandomResult}>Add Random Result</button>
-            <button onClick={clearResults}>Clear Results</button>
+            <section style={{
+                height: "5vh",
+                marginBottom: 0,
+                textAlign: "center",
+            }}>
+                <button onClick={addRandomResult}>Add Random Entry</button>
+                <button onClick={clearResults}>Clear Results</button>
+            </section>
         </div>
     );
 }
