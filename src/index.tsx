0ce4148b7117661718fa067786c96f447fcebddc
1toldyou <86680163+1toldyou@users.noreply.github.com>
2024-04-27 17:19:38 -0700

    use react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
 import './index.css';
 import App from './App';
 import {ChatStyleInterpreterPage} from "./pages";
 import reportWebVitals from './reportWebVitals';
 
+
+const browserRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "/chat-style-interpreter",
+        element: <ChatStyleInterpreterPage />
+    }
+]);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      {/*<App />*/}
-      <ChatStyleInterpreterPage />
+      <RouterProvider router={browserRouter} />
   </React.StrictMode>
 );
 
