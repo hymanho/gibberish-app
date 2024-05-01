6ac48351330f4348acbbc57579e024a8018e96a5
r7-1700 <hymanho@outlook.com>
2024-04-30 22:30:28 -0700

    Front-end interperter page

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,117 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( // here
+    <div className="App"> 
+
+      <header className="App-header"> 
+        
+        <header className = "Phone-screen">
+
+          <header className = "App-arrow-left">
+            <p>
+              →
+            </p>
+          </header>
+
+          <header className = "App-arrow-right">
+            <p>
+              ←
+            </p>
+          </header>
+
+
+          <header className = "App-language-select-left">
+
+          <p>
+            &nbsp;Language select▼
+          </p>
+
+
+          </header>
+
+          <header className = "App-language-select-right">
+
+            <p>
+              &nbsp;Language select▼
+
+            </p>
+
+          
+          </header>
+
+          <header className = "Start-button">
+            <p>
+              Start
+            </p>
+          </header>
+
+          <header className = "App-title">
+            <div className = "App-settings-top">
+            ⎯
+
+              
+        
+            </div>
+
+            <div className = "App-settings-middle">
+
+            ⎯
+              
+        
+            </div>
+            <div className = "App-settings-bottom">
+
+            ⎯
+              
+        
+            </div>
+            
+            
+
+          </header>
+
+          <header className="App-text-for-title">
+            <p>
+            </p>
+                Gibberish App 
+            </header>
+
+
+          <header className = "Transmission-box">
+            <p> 
+              Transmission: 
+            </p>
+  
+            <header className = "Playback-button-Translation">
+              <p>
+                Playback
+
+              </p>
+            </header>
+
+          </header>
+
+          <header className = "Transcription-box">
+            <p>
+            Transcription: 
+            </p>
+            <header className = "Playback-button-Transcription">
+              <p>
+                Playback
+
+              </p>
+            </header>
+
+          </header>
+
+        </header>
+
+      </header> 
+
+    </div> // here 
   );
 }
 
 export default App;
+
